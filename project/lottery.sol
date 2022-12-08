d60f6162df9f63b751febe5ab9a44ff754793c7c
Aman Bisht <109150392+Aman-SB@users.noreply.github.com>
2022-12-08 16:29:33 +0530

    updated lottery project
    updated the comment of the function

--- project/lottery.sol
+++ project/lottery.sol
@@ -2,7 +2,11 @@
 
 pragma solidity >=0.8.0 <0.9.0;
 
+// smart contract for lottery project
+
 contract Lotter {
+    //only one centralise authority who is managing to check the winner
+    
     address public manager;
     address payable[] public participants;
 
@@ -10,24 +14,32 @@ contract Lotter {
     {
         manager=msg.sender;
     }
-
+    
+    //functon to receive payment from partcipants
+    
     receive() external payable
     {
         require(msg.value == 1 ether);
         participants.push(payable(msg.sender));
     }
 
+    //function to check balance
+    
     function getBalance() public view returns(uint)
     {
         require(msg.sender == manager);
         return address(this).balance;
     }
-
+    
+    //function to select random number
+    
     function random() public view returns(uint)
     {
         return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
     }
-
+    
+    //function to select the winner
+    
     function selectWinner() public 
     {
         address winner;
